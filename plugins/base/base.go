4f977a807f20d57a70f13ac3caf0e2b0d01175d5
nich0s <noreply@github.com>
2020-06-06 10:40:59 -0500

    wip: safety save.

--- plugins/base/base.go
+++ plugins/base/base.go
@@ -1,6 +1,8 @@
 package base
 
 import (
+	"fmt"
+	"regexp"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
@@ -9,12 +11,19 @@ import (
 // Handle - the word Handle
 func Handle(s *discordgo.Session, e *discordgo.Event) {
 	if e.Type == "MESSAGE_CREATE" {
-		event := e.Struct.(*discordgo.MessageCreate)
-		msgSplice := strings.Fields(event.Content)
+		mc := e.Struct.(*discordgo.MessageCreate)
+		msgSplice := strings.Fields(mc.Content)
+
+		if len(msgSplice) == 0 {
+			// if the message is nil, fail safely
+			return
+		}
 
 		switch msgSplice[0] {
 		case ".echo":
-			echoMessage(event, s, strings.Join(msgSplice[1:], " "))
+			echoMessage(mc, s, strings.Join(msgSplice[1:], " "))
+		case ".slap":
+			troutSlap(mc, s)
 		}
 	}
 }
@@ -24,4 +33,47 @@ func echoMessage(m *discordgo.MessageCreate, s *discordgo.Session, content strin
 		return
 	}
 	s.ChannelMessageSend(m.ChannelID, content)
+}
+
+func troutSlap(m *discordgo.MessageCreate, s *discordgo.Session) {
+	// required check to disallow bot looping
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	// check to see that targets are present: "command @target"
+	c := strings.Fields(m.Content)
+	if len(c) < 2 {
+		fmt.Println("not enough args.")
+		return
+	}
+
+	// get user ID from content
+	u, err := getUserFromContent(s, c[1])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("_slaps %s around a bit with a large trout._", u.Mention()))
+}
+
+func getUserFromContent(s *discordgo.Session, data string) (user *discordgo.User, err error) {
+	rex, err := regexp.Compile("[^0-9]+")
+	if err != nil {
+		return nil, err
+	}
+
+	pu := rex.ReplaceAllString(data, "")
+
+	uo, err := s.User(pu)
+	if err != nil {
+		return nil, err
+	}
+
+	if uo == nil {
+		return nil, fmt.Errorf("unable to find a user, value=%s", data)
+	}
+
+	return uo, nil
 }
