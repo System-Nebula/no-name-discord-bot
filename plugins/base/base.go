5e07df37260abc4f86686d73a0de3f436c2473de
Myles <landais.myles@gmail.com>
2020-06-06 06:10:43 -0500

    POC - IsRoleMember checking membership permissions

--- plugins/base/base.go
+++ plugins/base/base.go
@@ -14,8 +14,14 @@ func Handle(s *discordgo.Session, e *discordgo.Event) {
 
 		switch msgSplice[0] {
 		case ".echo":
+			// check user permissions here or in function?
 			echoMessage(event, s, strings.Join(msgSplice[1:], " "))
+		case ".roles":
+			rolesMessage(event, s)
+		case ".fbi":
+			fbiMessage(event, s)
 		}
+
 	}
 }
 
@@ -24,4 +30,44 @@ func echoMessage(m *discordgo.MessageCreate, s *discordgo.Session, content strin
 		return
 	}
 	s.ChannelMessageSend(m.ChannelID, content)
+}
+
+func IsRoleMember(s *discordgo.Session, UID string, GID string, RoleName string) bool {
+	U, _ := s.GuildMember(GID, UID)
+	GuildRoles, _ := s.GuildRoles(GID)
+	for _, e := range U.Roles {
+		for _, r := range GuildRoles {
+			if e == r.ID {
+				if r.Name == RoleName {
+					return true
+				}
+			}
+		}
+	}
+	return false
+}
+
+func fbiMessage(m *discordgo.MessageCreate, s *discordgo.Session) {
+	b := IsRoleMember(s, m.Author.ID, m.GuildID, "FBI")
+	if b == true {
+		s.ChannelMessageSend(m.ChannelID, "Its not safe to talk here..")
+	} else {
+		s.ChannelMessageSend(m.ChannelID, "Your not with the FBI..")
+	}
+}
+
+func rolesMessage(m *discordgo.MessageCreate, s *discordgo.Session) {
+
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	User, _ := s.GuildMember(m.GuildID, m.Author.ID)
+	GuildRoles, _ := s.GuildRoles(m.GuildID)
+	for _, e := range User.Roles {
+		for _, r := range GuildRoles {
+			if e == r.ID {
+				s.ChannelMessageSend(m.ChannelID, r.Name)
+			}
+		}
+	}
 }
